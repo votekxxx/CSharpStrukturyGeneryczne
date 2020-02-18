eac9f8f2b0dd79bc1eca735c09da31077a2cc321
Wojciech Glijer <wojciech.glijer@gmail.com>
2020-02-18 15:03:13 +0100

    utworzenie projektu

---
+++ CSharpStrukturyGeneryczne/1_TypyGeneryczne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_TypyGeneryczne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
